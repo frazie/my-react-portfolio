6d27c486710ded447b4734f9c7fa403a6aa2a7e1
Frazier Odhiambo <ofrazy@gmail.com>
2023-04-03 23:37:11 +0300

    about page structure

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,6 +1,7 @@
 import Head from 'next/head'
 import Header from '@/components/Header'
 import Hero from '@/components/Hero'
+import About from '@/components/About'
 
 export default function Home() {
   return (
@@ -12,12 +13,14 @@ export default function Home() {
       {/*header */}
         <Header />
       {/*hero */}
-      <section id="hero" className='snap-center'>
+      <section id="hero" className='snap-start'>
         <Hero />
       </section>
 
       {/*about*/}
-
+      <section id='about' className='snap-center'>
+        <About />
+      </section>
       {/*experience */}
 
       {/*skills */}
