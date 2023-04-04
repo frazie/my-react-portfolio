47cd13d54e406d50201893a5ca818f9ae7b3b7f5
Frazier Odhiambo <ofrazy@gmail.com>
2023-04-04 15:07:26 +0300

    completed the about section

--- src/components/About.tsx
+++ src/components/About.tsx
@@ -2,16 +2,24 @@ import React from 'react'
 import { motion } from 'framer-motion'
 
 import Aboutimage from './Aboutimage'
+import Abouttext from './Abouttext'
 
 type Props = {}
 
 function About({}: Props) {
   return (
-    <div className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
-      <h3 className='absolute top-24 uppercase tracking-[20px] text-gray-300 text-2xl'>About</h3>
+    <motion.div
+    initial={{ opacity: 0 }}
+    whileInView={{opacity: 1}}
+    transition={{duration: 1.5}}
+    className='flex flex-col relative h-screen text-center md:text-left md:flex-row max-w-7xl px-10 justify-evenly mx-auto items-center'>
+      <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-300 text-2xl'>About</h3>
 
       <Aboutimage />
-    </div>
+
+      <Abouttext />
+      
+    </motion.div>
   )
 }
 
--- src/components/Aboutimage.tsx
+++ src/components/Aboutimage.tsx
@@ -6,10 +6,19 @@ type Props = {}
 function Aboutimage({}: Props) {
   return (
     <motion.img 
+    className='-mb-20 md:mb-0 flex-shrink-0 w-56 h-56 rounded-full  object-cover md:rounded-lg md:w-64 md:h-95 xl:w-[300px] xl:h-[400px]' 
     initial={{
-        x: -200,
-        
+        x: -300,
+        opacity: 0,
     }}
+    whileInView={{
+        x: 0,
+        opacity: 1,
+    }}
+    transition={{
+        duration: 1.2,
+    }}
+    viewport={{once: true}}
 
     src="https://frazier-se.netlify.app/assets/img/IMG_20180120_131810_874%20(1).jpg" 
     alt="about frazier image" />
