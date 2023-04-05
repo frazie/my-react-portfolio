9b3adf2e4144c5903947009fd208978f5ae47e61
Frazier Odhiambo <ofrazy@gmail.com>
2023-04-06 00:03:48 +0300

    slight change

--- src/components/Skill.tsx
+++ src/components/Skill.tsx
@@ -13,7 +13,7 @@ function Skill({ directionLeft }: Props) {
                 x: directionLeft ? -200 : 200, 
             opacity: 0,
             }}
-            transition={{duration: 1}}
+            transition={{duration: 1.5}}
             whileInView={{opacity: 1, x: 0}}
             src="https://www.sanity.io/static/images/logo_rounded_square.png" alt="" 
             className='rounded-full border-gray-500 object-cover w-24 h-24 md:h-28 md:w-28 xl:w-32 xl:h-32 filter group-hover:grayscale transition duration-300 ease-in-out'
