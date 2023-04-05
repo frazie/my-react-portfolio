01a20dd5381cb024ccb520088f8d347cbc125ad4
Frazier Odhiambo <ofrazy@gmail.com>
2023-04-06 00:01:24 +0300

    skills section not completed full but included

--- src/components/Experience.tsx
+++ src/components/Experience.tsx
@@ -10,11 +10,11 @@ function Experience({}: Props) {
     whileInView={{opacity: 1}}
     transition={{ duration: 1.5}}
     className='h-screen flex relative overflow-hidden flex-col text-left md:flex-row max-w-full px-10 justify-evenly mx-auto items-center '>
-        <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-300 text-2xl'>
+        <h3 className='absolute top-20 uppercase tracking-[20px] text-gray-500 text-2xl'>
             Experience
         </h3>
 
-        <div className='w-full flex space-x-5 overflow-x-scroll p-10 snap-x snap-mandatory' >
+        <div className='w-full flex space-x-5 overflow-x-scroll p-5 snap-x snap-mandatory' >
             {/* experience cards will come here */}
             <ExperienceCard />
             <ExperienceCard />
