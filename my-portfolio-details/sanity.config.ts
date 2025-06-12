fe4191215a2881ae7a1adfbdfa7a4442b71e5dd2
frazie <ofrazy@gmail.com>
2025-06-12 16:33:53 +0300

    sanity work

--- my-portfolio-details/sanity.config.ts
+++ my-portfolio-details/sanity.config.ts
@@ -1,6 +1,5 @@
 import {defineConfig} from 'sanity'
 import {deskTool} from 'sanity/desk'
-import {visionTool} from '@sanity/vision'
 import {schemaTypes} from './schemas'
 
 export default defineConfig({
